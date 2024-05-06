90b0df5f3965b61411f5f25f0e7aaeac0df319a1
Maria CHEREL SOROKINA <mcherel-@paul-f3Ar5s1.clusters.42paris.fr>
2024-05-06 17:13:08 +0200

    some tests

--- TRAINING/training.py
+++ TRAINING/training.py
@@ -21,6 +21,7 @@ def open_csv(file):
             X.append(float(km))
             y.append(float(price))
     return X, y
+
 def gradient_descent(theta0, theta1, X_km, y_price):
     learning_rate = 0.0000000001
     num_iterations = 1000
@@ -41,14 +42,73 @@ def gradient_descent(theta0, theta1, X_km, y_price):
 
     return theta0, theta1
 
-X_km, y_price = open_csv(file)
-theta0, theta1 = gradient_descent(theta0, theta1, X_km, y_price)
+def main():
+    X_km, y_price = open_csv(file)
+    theta0, theta1 = gradient_descent(theta0, theta1, X_km, y_price)
 
-with open(training_data_file, 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(['theta0', 'theta1'])
-    writer.writerow([theta0, theta1])
+    with open(training_data_file, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['theta0', 'theta1'])
+        writer.writerow([theta0, theta1])
+    print(theta0)
+    print(theta1)
+
+if __name__ == '__main__':
+    main()
+
+'''
+def cross_validation(X, y, learningRate, iterations, num_splits=5):
+    mae_scores = []
+    mse_scores = []
+    rmse_scores = []
+    r2_scores = []  # Corrected variable name
+    for _ in range(num_splits):
+        # Splitting the data into training and testing sets
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=None)
+        
+        # Training the model using gradient descent
+        t0, t1, _, _, _ = gradientDescent(X_train, y_train, learningRate, iterations)
+        
+        # Making predictions on the test set
+        y_pred = [t1 * normalizeElem(X_train, x) + t0 for x in X_test]
+        
+        # Calculating evaluation metrics
+        mae = metrics.mean_absolute_error(y_test, y_pred)
+        mae_scores.append(mae)
+        
+        mse = metrics.mean_squared_error(y_test, y_pred)
+        mse_scores.append(mse)
+        
+        rmse = np.sqrt(metrics.mean_squared_error(y_test, y_pred))
+        rmse_scores.append(rmse)
+        
+        r2 = metrics.r2_score(y_test, y_pred)  # Corrected variable name
+        r2_scores.append(r2)
+    return sum(mae_scores) / num_splits, sum(mse_scores) / num_splits, sum(rmse_scores) / num_splits, sum(r2_scores) / num_splits
 
 
-print(theta0)
-print(theta1)
+
+	
+def	main():
+	learningRate = 0.5
+
+	iterations = 1000
+	
+	mileages, prices = getData(getPath('data.csv'))
+	x, y = normalizeData(mileages, prices)
+	t0, t1, lossHistory, t0History, t1History = gradientDescent(x, y, learningRate, iterations)
+	storeData(t0, t1, 'thetas.csv')
+	displayPlot(t0, t1, mileages, prices, lossHistory, t0History, t1History)
+	# Dans votre fonction main()
+	avg_mae, avg_mse, avg_rmse, avg_r2 = cross_validation(x, y,learningRate, iterations)
+
+	print(f'MAE: \t{avg_mae}')
+	print(f'MSE: \t{avg_mse}')
+	print(f'RMSE: \t{avg_rmse}')
+	print(f'R²: \t{avg_r2}')
+
+
+	
+if	__name__ == '__main__':
+	main()
+'''
