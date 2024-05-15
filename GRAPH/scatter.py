7ea1a3bbe4cea41999e826de15884ca1aa397817
Maria CHEREL SOROKINA <mcherel-@paul-f3Ar5s1.clusters.42paris.fr>
2024-05-15 17:39:41 +0200

    scatter

--- GRAPH/scatter.py
+++ GRAPH/scatter.py
@@ -26,11 +26,27 @@ if os.path.exists(file_path):
     with open(file=file_path, mode='r', encoding='utf-8') as csvfile:
         dataset = csv.reader(csvfile)
         next(dataset)
-        ordonne = np.linspace( 250000, 50000)
-        theta0 = 0
-        theta1 = 0
+        #ordonne = np.linspace( 250000, 50000)
         theta0, theta1 = map(float, next(dataset))
+        xmin, xmax = X.min(), X.max()
+        ordonne = np.linspace(xmin, xmax)
+        plt.plot(ordonne, theta0 + theta1 * ordonne, color='r')
+        #plt.plot(ordonne,  color='r')
+        #theta0 = 0
+        #theta1 = 0
+        #theta0, theta1 = map(float, next(dataset))
         #plt.plot(ordonne, theta0 * ordonne + theta0, color='r')
 
 plt.show()
 
+"""
+file_path = "../DATA/training.csv"
+theta0 = 0
+theta1 = 0
+
+if os.path.exists(file_path):
+    with open(file=file_path, mode='r', encoding='utf-8') as csvfile:
+        dataset = csv.reader(csvfile)
+        next(dataset)
+        theta0, theta1 = map(float, next(dataset)
+        """
