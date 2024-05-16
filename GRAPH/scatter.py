d909a58141fcabbd71bc190166f081fc0b9fb9e6
Maria CHEREL SOROKINA <mcherel-@paul-f3Ar3s10.clusters.42paris.fr>
2024-05-16 15:53:05 +0200

    conflict scatter and plot

--- GRAPH/scatter.py
+++ GRAPH/scatter.py
@@ -7,8 +7,53 @@ import os
 import csv
 import TOOLS as tools
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
 
 file_path = "../DATA/training.csv"
+data = pd.read_csv('../DATA/data.csv')
+X = data['km'].values.reshape(-1, 1)
+y = data['price'].values.reshape(-1, 1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=0)
+plt.scatter(X,y)
+regressor = LinearRegression()
+#Training
+regressor.fit(X_train, y_train)
+regressor = LinearRegression()
+#Training
+regressor.fit(X_train, y_train)
+theta11 = regressor.coef_
+theta00 = regressor.intercept_
+
+with open(file=file_path, mode='r', encoding='utf-8') as csvfile:
+        dataset = csv.reader(csvfile)
+        next(dataset)
+        theta0, theta1 = map(float, next(dataset))
+        theta1 = [[theta1]]
+        theta0 = [theta0]
+
+        print(f'{theta11} {theta1}')
+        print(f'{theta00} {theta0}')
+# Adding a red line
+xmin, xmax = X.min(), X.max()
+ordonne = np.linspace(xmin, xmax)
+#ordonne = np.linspace(0,11,1000)#creates a table of 1000 values from 0 to 15
+#graph plot
+plt.plot(ordonne,theta1[0]*ordonne + theta0, color='r')
+
+# Prediction from test base
+y_predict = regressor.predict(X_test)
+
+# Metrics
+print(f'MAE: \t{metrics.mean_absolute_error(y_test, y_predict)}')
+print(f'MSE: \t{metrics.mean_squared_error(y_test, y_predict)}')
+print(f'RMSE: \t{np.sqrt(metrics.mean_squared_error(y_test, y_predict))}')
+print(f'R²: \t{metrics.r2_score(y_test, y_predict)}')
+
+plt.show()
+
+""" file_path = "../DATA/training.csv"
 data = pd.read_csv('../DATA/data.csv')
 
 X = data['km'].values.reshape(-1, 1)
@@ -34,6 +79,6 @@ if os.path.exists(file_path):
         plt.scatter(X, y, label='Données', color='b')
         
         plt.legend()
-        plt.title('Price Prediction')
+        plt.title('Price Prediction') """
 
 plt.show()
