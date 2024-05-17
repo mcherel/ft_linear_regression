10af691e3b0769825dcd97d31d3de7f8ef215b1a
Maria CHEREL SOROKINA <mcherel-@paul-f3Ar9s1.clusters.42paris.fr>
2024-05-17 15:40:04 +0200

    prompt validation

--- ESTIMATE_PRICE/prompt.py
+++ ESTIMATE_PRICE/prompt.py
@@ -5,7 +5,14 @@ from TOOLS import estimate_price as price
 import csv
 import TOOLS as tools
 
-km = int(input("Please, enter the milage in kilometers: \n"))
+km = input("Please, enter the milage in kilometers: \n")
+try:
+    km = int(km)
+    if not (0 <= km <= 250000):
+        raise ValueError
+except ValueError:
+    print("Sorry, the asked number should be an int between 0 and 250000")
+    exit()
 
 file_path = "../DATA/training.csv"
 theta0 = 0
@@ -16,4 +23,5 @@ if os.path.exists(file_path):
         dataset = csv.reader(csvfile)
         next(dataset)
         theta0, theta1 = map(float, next(dataset))
-print(f"The price of you car is estimated to {int(tools.estimate_price.hypothesis(theta0, theta1, km))}")
+        estimated_price = tools.estimate_price.hypothesis(theta0, theta1, km)
+print(f"The price of you car is estimated to {int(estimated_price)}")
